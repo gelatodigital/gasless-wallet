7402beeae995532aca95274d8337953cf08ac273
opatavi <pataviogulcan@gmail.com>
2023-02-24 15:02:04 +0300

    feat: whitelisting domains

--- packages/gasless-onboarding/src/types/index.ts
+++ packages/gasless-onboarding/src/types/index.ts
@@ -2,6 +2,7 @@ import { LoginSettings } from "@web3auth/openlogin-adapter";
 
 export interface LoginConfig {
   chain: { id: number; rpcUrl: string };
+  domains: string[];
   ui?: {
     theme?: "light" | "dark";
   };
---
+++ packages/gasless-onboarding/src/utils/index.ts
@@ -0,0 +1,2 @@
+export { getSignatures } from "./getSignatures";
+export { getHttpErrorMessage } from "./getHttpErrorMessage";
